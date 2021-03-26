f9e6519408f2b011b629d4dacdac8b7e6aa014a0
michirem <dariuss@gmx.at>
2021-03-26 09:36:35 +0100

    Created page components and completed routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeroComponent } from './hero/hero.component';
+import { BlogComponent } from './blog/blog.component';
+import { HomeComponent } from './home/home.component';
+import { TravelsComponent } from './travels/travels.component';
+import { FooterComponent } from './footer/footer.component';
+import { TravelDetailsComponent } from './travel-details/travel-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeroComponent,
+    BlogComponent,
+    HomeComponent,
+    TravelsComponent,
+    FooterComponent,
+    TravelDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/travel-details/travel-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travel-details',
+  templateUrl: './travel-details.component.html',
+  styleUrls: ['./travel-details.component.scss']
+})
+export class TravelDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
